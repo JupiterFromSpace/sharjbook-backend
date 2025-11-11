498784c32817642e7933d9aaae1a7dbac30d188e
sina <sinamatari23@gmail.com>
2025-11-11 09:35:03 +0330

    basic files (apps) created

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "corsheaders",
+    'accounts',
+    'buildings',
+    'finance',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
