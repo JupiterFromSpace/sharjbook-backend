31b92a28153e92d702c38f7e42b6519778747241
sina <sinamatari23@gmail.com>
2025-12-31 16:08:17 +0330

    serializer accounts debuged for develope

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -5,9 +5,17 @@ class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("phone",)
+        extra_kwargs = {
+            "phone": {
+                "validators":[]
+            }
+        }
 
     def create(self, validated_data):
         phone = validated_data["phone"]
-        user, created = User.objects.get_or_create(phone=phone)
-        return user
 
+        user, created = User.objects.get_or_create(phone=phone)
+
+        self.context["created"] = created
+
+        return user
